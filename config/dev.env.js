6fe7ab948954a1b90dd3031688a507c3d2596486
DDD <www.124780540@qq.com>
2017-12-15 23:48:42 +0800

    Login

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+// https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"http://localhost:3000"'
+})
